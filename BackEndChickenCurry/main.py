41f3b1a5b792dddac7169d86d63588350b25d840
Malit-31 <lazarlazar3482035@gmail.com>
2024-07-28 21:51:35 -0700

    sharma is a carry Co-authored-by: Rohan-Chilukuri <Rohan-Chilukuri@users.noreply.github.com> Co-authored-by: wut da hail <gamedoescool@users.noreply.github.com>

--- BackEndChickenCurry/main.py
+++ BackEndChickenCurry/main.py
@@ -1,16 +1,35 @@
-from flask import request, jsonify
-from config import app, db
-from user import User
+from flask import Flask, request, jsonify
+from flask import json
+from flask_cors import CORS
 
-@app.route("/", methods=["GET"])
-def get_contacts():
-    contacts = User.query.all()
-    json_info = list(map(lambda x: x.json_write(), contacts))
-    return jsonify({"contacts": json_info})
+#freddy fazbear's pizzaria har har har har har har har har har
+#freddy fazbear's pizzaria har har har har har har har har har
 
-
+userdataPath = "freddyFazbear.json"
+app = Flask(__name__)
+CORS(app)
+@app.route('/savedata', methods=['POST'])
+def saveData(): # {"name": {"password":<password>}}
+    send = request.get_json()
+    with open(userdataPath, 'r') as f:
+        filedata = json.load(f)
+    filedata.update(send)
+    with open(userdataPath, 'w') as f:
+        json.dump(filedata, f)
+    return jsonify({"status":"posted"}) 
+@app.route('/login', methods=['GET'])
+def login():
+    result = request.args.get('total').split(",")  # "name,password"
+    with open(userdataPath,'r') as file:
+        data = json.load(file)
+    if result[0] in data:
+        if result[1] == data[result[0]]['password']:
+            print("har har har har har har har har har har har har")
+            return jsonify({"status":"success"})
+        return jsonify({"status":"wrong password"})
+    return jsonify({"status":"wrong username"})
 if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
-    app.run(debug=True)
+    app.run()
 
+#freddy fazbear's pizzaria har har har har har har har har har
+#freddy fazbear's pizzaria har har har har har har har har har    
