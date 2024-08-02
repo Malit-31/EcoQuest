05bcc4024ee3588ea5d805ad9aef3902d3071f8a
Malit-31 <lazarlazar3482035@gmail.com>
2024-08-02 12:56:59 -0700

    har

--- templates/app.js
+++ templates/app.js
@@ -1,11 +1,11 @@
 function myFunction(){
-    fetch(IP, {
-        method:"POST",
-        headers: {"Content-Type":"application/json"},
-        body: JSON.stringify({"password":69420,"username":"harharhar"})
-     }).then(response => response.json()).then(data => {
-         //do tanning with data
-     })
+    // fetch(IP, {
+    //     method:"POST",
+    //     headers: {"Content-Type":"application/json"},
+    //     body: JSON.stringify({"password":69420,"username":"harharhar"})
+    //  }).then(response => response.json()).then(data => {
+    //      //do tanning with data
+    //  })
     if (document.getElementById("username").value!=="" && document.getElementById("password").value!=="") {
         window.location.href="questclaim.html"
     }
