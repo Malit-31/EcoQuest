4868c51bfdd6eaa4ee92b78b56b7fdd3d642b9ef
Malit-31 <lazarlazar3482035@gmail.com>
2024-08-03 13:58:32 -0700

    e

--- templates/app.js
+++ templates/app.js
@@ -1,40 +1,7 @@
-function login() {
-    const username = document.getElementById("username").value;
-    const password = document.getElementById("password").value;
-
+function MyFunction() {
     if (username !== "" && password !== "") {
-        fetch("http://127.0.0.1:5000/login?total={username},{password}")
-            .then(response => response.json())
-            .then(users => {
-                const user = users.find(user => user.username === username && user.password === password);
-                if (user) {
-                    window.location.href = "questclaim.html";
-                } else {
-                    alert("wrong username and password");
-                }
-            })
-            .catch(error => console.error('Error fetching users:', error));
+        window.location.href = "link.html"
     } else {
-        alert("Please fill out all fields");
-    }
-}
-function createacc(){
-    const username = document.getElementById("username").value;
-    const password = document.getElementById("password").value;
-
-    if (username !== "" && password !== "") {
-        fetch("http://127.0.0.1:5000/savedata")
-            .then(response => response.json())
-            .then(users => {
-                const user = users.find(user => user.username === username && user.password === password);
-                if (user) {
-                    window.location.href = "index.html";
-                } else {
-                    alert("wrong username and password");
-                }
-            })
-            .catch(error => console.error('Error fetching users:', error));
-    } else {
-        alert("Please fill out all fields");
+        //har
     }
 }
