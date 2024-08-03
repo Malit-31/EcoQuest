ea29637cbf2842f33cbd7909e4a73e6f9a94aef3
Malit-31 <lazarlazar3482035@gmail.com>
2024-08-03 12:18:08 -0700

    ehehhehe

--- templates/app.js
+++ templates/app.js
@@ -3,7 +3,7 @@ function login() {
     const password = document.getElementById("password").value;
 
     if (username !== "" && password !== "") {
-        fetch("users.json")
+        fetch("http://127.0.0.1:5000/savedata")
             .then(response => response.json())
             .then(users => {
                 const user = users.find(user => user.username === username && user.password === password);
@@ -17,4 +17,7 @@ function login() {
     } else {
         alert("Please fill out all fields");
     }
+}
+function createacc(){
+    
 }
